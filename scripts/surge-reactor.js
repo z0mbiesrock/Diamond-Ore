1ab8be009c7a5c3573322016c2f5af8385cbe96d
z0mbiesrock <z0mbiesrock@aol.com>
2020-02-20 12:03:20 -0500

    update surge-reactor.js

--- scripts/surge-reactor.js
+++ scripts/surge-reactor.js
@@ -2,31 +2,32 @@
 
 const surgeReactorMeltdownFlak = extend(MissileBulletType, {});
 
-surgeReactorMeltdownFlak.speed = 9;
+surgeReactorMeltdownFlak.speed = 8;
 //this does 800 damage per 5 ticks btw.
-surgeReactorMeltdownFlak.damage = 250;
+surgeReactorMeltdownFlak.damage = 500;
 surgeReactorMeltdownFlak.bulletWidth = 16;
-surgeReactorMeltdownFlak.bulletHeight = 24;
+surgeReactorMeltdownFlak.bulletHeight = 200;
+surgeReactorMeltdownFlak.bulletShrink = 1;
 surgeReactorMeltdownFlak.lifetime = 60;
 surgeReactorMeltdownFlak.hitEffect = Fx.hitMeltdown;
 surgeReactorMeltdownFlak.despawnEffect = Fx.hitMeltdown;
 surgeReactorMeltdownFlak.hitSize = 5;
 surgeReactorMeltdownFlak.lightining = 5;
-surgeReactorMeltdownFlak.lightningLength = 10;
-surgeReactorMeltdownFlak.weaveScale = 3;
-surgeReactorMeltdownFlak.weaveMag = 3;
+surgeReactorMeltdownFlak.lightningLength = 25;
 surgeReactorMeltdownFlak.pierce = true;
-surgeReactorMeltdownFlak.bulletSprite = "shell";
+surgeReactorMeltdownFlak.homingPower= 0;
+surgeReactorMeltdownFlak.homingRange= 0;
+surgeReactorMeltdownFlak.bulletSprite = "bullet";
 surgeReactorMeltdownFlak.frontColor = Color.valueOf("#ffffcc");
-surgeReactorMeltdownFlak.frontColor = Color.valueOf("#ffffcc");
-surgeReactorMeltdownFlak.trailColor = Color.valueOf("#ffffcc");
+surgeReactorMeltdownFlak.backColor = Color.valueOf("#ffff00");
+surgeReactorMeltdownFlak.trailColor = Color.valueOf("#ffffff");
 
 //create a simple shockwave effect
 const surgeReactorMeltdownBlast = extend(BasicBulletType, {});
 
 surgeReactorMeltdownBlast.speed = 0.001;
 //this does 800 damage per 5 ticks btw.
-surgeReactorMeltdownBlast.damage = 500;
+surgeReactorMeltdownBlast.damage = 2000;
 surgeReactorMeltdownBlast.drawSize = 480;
 surgeReactorMeltdownBlast.lifetime = 1;
 surgeReactorMeltdownBlast.hitEffect = Fx.hitMeltdown;
@@ -35,10 +36,13 @@ surgeReactorMeltdownBlast.hitSize = 5;
 surgeReactorMeltdownBlast.lightining = 5;
 surgeReactorMeltdownBlast.lightningLength = 25;
 surgeReactorMeltdownBlast.pierce = true;
-surgeReactorMeltdownBlast.fragVelocityMin = 0.15;
-surgeReactorMeltdownBlast.fragVelocityMax = 1.75;
-surgeReactorMeltdownBlast.fragBullets = 20;
-surgeReactorMeltdownBlast.fragBullet = surgeReactorMeltdownFlak;
+/* surgeReactorMeltdownBlast.fragVelocityMin = 0.25;
+surgeReactorMeltdownBlast.fragVelocityMax = 2.55;
+surgeReactorMeltdownBlast.fragBullets = 40;
+surgeReactorMeltdownBlast.fragBullet = surgeReactorMeltdownFlak; */
+surgeReactorMeltdownBlast.frontColor = Color.valueOf("#ffffcc");
+surgeReactorMeltdownBlast.backColor = Color.valueOf("#ffff00");
+surgeReactorMeltdownBlast.trailColor = Color.valueOf("#ffffff");
 
 const SurgeReactor = extendContent(NuclearReactor, "surge-reactor", {
 	
@@ -52,17 +56,20 @@ const SurgeReactor = extendContent(NuclearReactor, "surge-reactor", {
     //OVERRIDE
 	onDestroyed: function(tile){
 		Blocks.thoriumReactor.onDestroyed(tile);
+        for(var i = 0; i < 45; i++){
+            Calls.createBullet(surgeReactorMeltdownFlak, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.15, 1.0), Mathf.random(0.2, 1.0))
+		}
 		surgeReactorMeltdownBlast.lightningLength = 25
-        Calls.createBullet(surgeReactorMeltdownBlast, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
-		surgeReactorMeltdownBlast.lightningLength = 30
         Calls.createBullet(surgeReactorMeltdownBlast, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
 		surgeReactorMeltdownBlast.lightningLength = 35
         Calls.createBullet(surgeReactorMeltdownBlast, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
-		surgeReactorMeltdownBlast.lightningLength = 40
-        Calls.createBullet(surgeReactorMeltdownBlast, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
 		surgeReactorMeltdownBlast.lightningLength = 45
         Calls.createBullet(surgeReactorMeltdownBlast, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
-		surgeReactorMeltdownBlast.lightningLength = 50
+		surgeReactorMeltdownBlast.lightningLength = 55
+        Calls.createBullet(surgeReactorMeltdownBlast, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
+		surgeReactorMeltdownBlast.lightningLength = 65
+        Calls.createBullet(surgeReactorMeltdownBlast, Team.derelict, tile.worldx(), tile.worldy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
+		surgeReactorMeltdownBlast.lightningLength = 75
 	},
 })
 /* surgeReactor.itemCapacity: 100
@@ -77,7 +84,7 @@ surgeReactor.flashThreshold: 0.75
 surgeReactor.coolantPower: 0.225
 surgeReactor.buildCostMultiplier: 0.73227
 surgeReactor.powerProduction: 166.66666666666666666666666666667
+ */
 surgeReactor.lightColor = Color.valueOf("7f19ea");
 surgeReactor.coolColor = Color.valueOf("ffff00a3");
 surgeReactor.hotColor = Color.valueOf("ff9575a3");
-surgeReactor.update: true */
