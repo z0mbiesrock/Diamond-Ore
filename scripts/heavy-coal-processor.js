a5fe599843c5696a6032c38007b2c4cd2598a83b
z0mbiesrock <z0mbiesrock@aol.com>
2020-03-26 21:07:14 -0400

    wojak.exe

--- scripts/heavy-coal-processor.js
+++ scripts/heavy-coal-processor.js
@@ -5,14 +5,8 @@ extendContent(GenericCrafter, "heavy-coal-processor", {
 		Draw.alpha(tile.entity.liquids.currentAmount() / this.liquidCapacity);
 		Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
 		Draw.color();
-		if (rot = null){
-			rot = 0;
-		}
-		if (tile.entity.cons.valid()) {
-		rot += tile.entity.progress * Time.delta();
-		};
-        Draw.rect(Core.atlas.find(this.name + "-spinner-a"),tile.drawx(),tile.drawy(), -6 * rot);
-        Draw.rect(Core.atlas.find(this.name + "-spinner-b"),tile.drawx(),tile.drawy(), 6 * rot);
+        Draw.rect(Core.atlas.find(this.name + "-spinner-a"),tile.drawx(),tile.drawy(), -180 * tile.entity.progress);
+        Draw.rect(Core.atlas.find(this.name + "-spinner-b"),tile.drawx(),tile.drawy(), 180 * tile.entity.progress);
 		Draw.reset();
 	},
     generateIcons(){
