57663f887be04d204e16d49f86af27fb1460c663
z0mbiesrock <z0mbiesrock@aol.com>
2020-04-06 17:26:35 -0400

    cleanup

--- scripts/units/spiker.js
+++ scripts/units/spiker.js
@@ -5,10 +5,21 @@ spikerUnit.create(prov(() => new JavaAdapter(GroundUnit, {
 	update(){
 		this.super$update();
 		// Make nearest enemy the movement target
-		this.target = Units.closestTarget(this.getTeam(), this.x, this.y, 300)
-		if (this.target != null){
-		tile = world.tileWorld(this.target.x, this.target.y);
-		targetTile = Pathfinder.getTargetTile(tile, this.getTeam(), this.target)
+		this.meleeTarget = Units.closestTarget(this.getTeam(), this.x, this.y, 300);
+		if(this.meleeTarget != null && (this.meleeTarget.isDead() || this.meleeTarget.dst(this) > 300)){
+            this.meleeTarget = null;
+        }
+		if (this.meleeTarget != null){
+			vel = Vec2(this.x, this.y);
+			try{
+				this.velocity().add(vel.trns(this.angleTo(this.meleeTarget), 0.25 * Time.delta()));
+				//this.target = this.meleeTarget
+				this.rotation = Mathf.slerpDelta(rotation, this.angleTo(this.meleeTarget), type.rotatespeed);
+			}
+			catch(error){
+				print(error);
+				this.target = this.meleeTarget;
+			}
 		}
 	},
 })));
