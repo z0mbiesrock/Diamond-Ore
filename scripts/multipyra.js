2760b95d36314f3d481442f0d8bcc09cdbc25b31
z0mbiesrock <z0mbiesrock@aol.com>
2020-04-06 12:42:39 -0400

    Converting original directory to git repository

--- scripts/multipyra.js
+++
@@ -1,9 +0,0 @@
-extendContent(GenericCrafter, "multipyra", {
-	draw(tile){
-		Draw.rect(this.region, tile.drawx(), tile.drawy());
-		if (tile.entity.cons.valid()) {
-			Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
-		};
-		Draw.reset();
-	}
-});
