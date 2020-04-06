f1eb99fdbb6a19294df19851a83cc1182ecbda07
z0mbiesrock <z0mbiesrock@aol.com>
2020-04-06 18:04:48 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,5 @@ require("blocks/spore-blocks")
 require("blocks/heavy-coal-processor")
 require("mechs/lambdaship")
 require("units/schadenfreude")
-require("units/fixer")
 require("units/spiker")
 
