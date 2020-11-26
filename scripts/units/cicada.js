8497696d9ac73a70923c1bb08fe1eb50fb0cff43
z0mbiesrock <z0mbiesrock@aol.com>
2020-11-26 09:21:16 -0500

    more cicada balancing

--- scripts/units/cicada.js
+++ scripts/units/cicada.js
@@ -59,6 +59,7 @@ SuiAirT5.constructor = () => extend(UnitEntity, {
 	}
 });
 SuiAirT5.ammoType = AmmoTypes.powerHigh;
+SuiAirT5.targetFlag = BlockFlag.reactor;
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "diamond-ore-scarab"), Vars.content.getByName(ContentType.unit, "diamond-ore-cicada")]);
 Blocks.tetrativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
