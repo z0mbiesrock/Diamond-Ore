93ffd18f152a83562eeeea13de5f125fa370b4b8
z0mbiesrock <z0mbiesrock@aol.com>
2020-11-28 11:38:46 -0500

    some changes

--- scripts/units/cicada.js
+++ scripts/units/cicada.js
@@ -60,6 +60,7 @@ SuiAirT5.constructor = () => extend(UnitEntity, {
 });
 SuiAirT5.ammoType = AmmoTypes.powerHigh;
 SuiAirT5.targetFlag = BlockFlag.reactor;
+SuiAirT5.payloadCapacity = 2200;
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "diamond-ore-scarab"), Vars.content.getByName(ContentType.unit, "diamond-ore-cicada")]);
 Blocks.tetrativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
